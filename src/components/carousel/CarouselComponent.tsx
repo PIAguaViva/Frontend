868e112e213cd1f469eab7f848475d77a3ec5f37
Brinamg <brina.gandra@gmail.com>
2022-03-09 17:18:52 -0300

    home

---
+++ src/components/carousel/CarouselComponent.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Carousel from 'react-elastic-carousel'           // Não esqueça de instalar a Dependencia
+
+import './CarouselComponent.css'
+
+function CarouselComponent() {
+    var items = [
+        { img: "https://i.imgur.com/bIhTBKQ.png" },
+        { img: "https://i.imgur.com/TIf8Rdk.png" },
+        { img: "https://i.imgur.com/eqUASVe.png" },
+        { img: "https://i.imgur.com/un7FAp7.png" },
+        { img: "https://i.imgur.com/r1hKlAa.png"}
+        // { img: { arcane } } // 
+    ]
+
+    return (
+        <Carousel isRTL={false}>
+            {
+                items.map(item => (
+                    <>
+                        <img src={item.img} alt="Item" className='imagem' />
+                    </>
+                ))
+            }
+        </Carousel>
+    )
+}
+
+export default CarouselComponent;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Typography, Box, Grid, Button } from '@material-ui/core';
+import { Box, Grid, Button } from '@material-ui/core';
 import TabPostagem from '../../components/postagens/tabPostagem/TabPostagem';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 import './Home.css';
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { TokenState } from '../../store/tokens/TokensReducer';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
+import CarouselComponent from '../../components/carousel/CarouselComponent';
 
 function Home() {
 
@@ -17,7 +18,7 @@ function Home() {
     )
 
     useEffect(() => {
-        if (token == "") {
+        if (token === "") {
             toast.info("Você precisa estar logado", {
                 position: "top-right",
                 autoClose: 2000,
@@ -34,28 +35,19 @@ function Home() {
     }, [token])
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
+            <Grid container justifyContent="center" alignItems="center" className='ft'>
+                    <Grid item xs={12}>
+                        <CarouselComponent/>
+                    </Grid>
+                    <Box display="flex" justifyContent="center" className='bot02'>
+                        <Box marginRight={15}>
                             <ModalPostagem />
                         </Box>
-                        <Link to = '/posts' className='text-decorator-none'>
-                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                        <Link to='/posts' className='text-decorator-none'>
+                            <Button variant="outlined" className='botaoo'>Ver Postagens</Button>
                         </Link>
                     </Box>
                 </Grid>
-                <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
-                </Grid>
-                <Grid xs={12} className='postagens'>
-                    <TabPostagem />
-                </Grid>
-            </Grid>
         </>
     );
 }
